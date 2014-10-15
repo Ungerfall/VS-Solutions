8090601b54fc37e7474296e9557d03ae06dce5f9
Ungerfall <ungerfall@gmail.com>
2014-10-15 12:30:32 +0400

    15.10.2014 12-30

---
+++ KonturKampus2014/KonturKampus2014/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace KonturKampus2014
+{
+    class Program
+    {
+        public class Point
+        {
+            public Point(int x, int y)
+            {
+                X = x;
+                Y = y;
+            }
+            public int X, Y;
+        }
+
+        public class Classroom
+        {
+            public List<string> Students = new List<string>();
+        }
+
+        public static int[] ParseNumbers(IEnumerable<string> lines)
+        {
+            return lines
+                .Where(w => w.Length > 0)
+                .Select(int.Parse)
+                .ToArray();
+        }
+
+        public static string[] GetAllStudents(Classroom[] classes)
+        {
+            return classes
+                .SelectMany(s => s.Students)
+                .ToArray();
+        }
+
+        public static List<Point> ParsePoints(IEnumerable<string> lines)
+        {
+            return lines
+                .Select(s =>
+                {
+                    var nums = s.Split(' ');
+                    return new Point(int.Parse(nums[0]), int.Parse(nums[1]));
+                })
+                .ToList();
+        }
+
+        public static string[] GetSortedWords(params string[] textLines)
+        {
+            return textLines
+                .Select(s => Regex.Split(s, @"\W+"))
+                .SelectMany(sm => sm.Where(w => !string.IsNullOrEmpty(w)))
+                .Select(s => s.ToLower())
+                .OrderBy(ob => ob)
+                .Distinct()
+                .ToArray();
+        }
+
+        static void Main()
+        {
+            var vocabulary = GetSortedWords(
+                "Hello, hello, hello, how low",
+                "",
+                "With the lights out, it's less dangerous",
+                "Here we are now; entertain us",
+                "I feel stupid and contagious",
+                "Here we are now; entertain us",
+                "A mulatto, an albino, a mosquito, my libido...",
+                "Yeah, hey"
+            );
+            foreach (var word in vocabulary)
+                Console.WriteLine(word);
+        }
+    }
+}
--- SOASerialization/Program.cs
+++ SOASerialization/Program.cs
@@ -1,9 +1,11 @@
 ﻿using System;
+using System.IO;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
+using System.Xml;
 using System.Xml.Serialization;
-using System.IO;
-using System.Web.Script.Serialization;
+using Newtonsoft.Json;
 
 namespace SOASerialization
 {
@@ -18,41 +20,44 @@ namespace SOASerialization
 
         public class Output
         {
+            public Output()
+            {
+                SumResult = SumResult;
+                MulResult = MulResult;
+                SortedInputs = SortedInputs;
+            }
+            public Output(Input input)
+            {
+                SumResult = input.Sums.Sum()*input.K;
+                MulResult = input.Muls.Aggregate(1, (a, b) => a*b);
+                SortedInputs = input.Sums.Concat(input.Muls.Select(Convert.ToDecimal)).OrderBy(t => t).ToArray();
+            }
             public decimal SumResult { get; set; }
             public int MulResult { get; set; }
             public decimal[] SortedInputs { get; set; }
         }
         
-        static void Main(string[] args)
-        {    
-            XmlSerializer xmlSerializer;
-            JavaScriptSerializer jsSerializer;
+        static void Main()
+        {
             Input input;
             Output output;
-
-            if (Console.ReadLine() == "Xml")
+            var sType = Console.ReadLine();
+            var text = Regex.Replace(Console.In.ReadToEnd(), @"\s+", string.Empty);
+            if (sType == "Xml")
             {
-                xmlSerializer = new XmlSerializer(typeof(Input));
-                input = xmlSerializer.Deserialize(Console.In) as Input;
-                output = new Output()
-                {
-                    SumResult = input.Sums.Sum() * input.K,
-                    MulResult = input.Muls.Aggregate(1, (a, b) => a * b),
-                    SortedInputs = input.Sums.Concat(input.Muls.Select(x => Convert.ToDecimal(x))).OrderBy(t => t).ToArray()
-                };
-                xmlSerializer.Serialize(Console.Out, output);
+                var xmlSerializer = new XmlSerializer(typeof(Input));
+                using (var stream = new MemoryStream(Encoding.UTF8.GetBytes(text)))
+                    input = (Input) xmlSerializer.Deserialize(stream);
+                output = new Output(input);
+                xmlSerializer = new XmlSerializer(typeof(Output));
+                using (var xmlWriter = XmlWriter.Create(Console.Out, new XmlWriterSettings {OmitXmlDeclaration = true}))
+                    xmlSerializer.Serialize(xmlWriter, output, new XmlSerializerNamespaces(new[] {XmlQualifiedName.Empty}));
             }
             else
             {
-                jsSerializer = new JavaScriptSerializer();
-                input = jsSerializer.Deserialize<Input>(Console.In.ToString());
-                output = new Output()
-                {
-                    SumResult = input.Sums.Sum() * input.K,
-                    MulResult = input.Muls.Aggregate(1, (a, b) => a * b),
-                    SortedInputs = input.Sums.Concat(input.Muls.Select(x => Convert.ToDecimal(x))).OrderBy(t => t).ToArray()
-                };
-                Console.Write(jsSerializer.Serialize(output));
+                input = JsonConvert.DeserializeObject<Input>(text);
+                output = new Output(input);
+                Console.Write(JsonConvert.SerializeObject(output));
             }
         }
     }
